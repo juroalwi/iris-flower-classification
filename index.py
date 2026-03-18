eba46ef05d1c625983ffe3b0b59c045534ac869d
Julian Alvarez <juroalwi@gmail.com>
2026-03-18 11:50:39 -0300

    sepparate into functions, add svm classification

--- index.py
+++ index.py
@@ -1,37 +1,41 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns
-import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
 
-columns = ['Sepal length', 'Sepal width', 'Petal length', 'Petal width', 'Class labels']
+data = sns.load_dataset("iris")
+X = data.drop("species", axis=1)
+y = data["species"]
 
-df = pd.read_csv('iris.data', names=columns)
 
-print('First rows:')
-print(df.head(), '\n')
+def plot_pairplots():
+    sns.pairplot(data, hue="species")
+    plt.savefig("pairplot.png", dpi=300, bbox_inches="tight")
 
-print('Data description:')
-print(df.describe(), '\n')
 
-# pair plots
-pairplot = sns.pairplot(df, hue='Class labels')
-plt.savefig('pairplot.png', dpi=300, bbox_inches='tight')
+def plot_barplots():
+    bar_width = 0.2
+    mean_values = np.array([X[y == j].mean() for j in y.unique()])
+    x_axis = np.arange(len(X.columns))
+    plt.figure()
+    plt.bar(x_axis, mean_values[0], bar_width, label="Setosa")
+    plt.bar(x_axis + bar_width, mean_values[1], bar_width, label="Versicolor")
+    plt.bar(x_axis + bar_width * 2, mean_values[2], bar_width, label="Virginica")
+    plt.xticks(x_axis + bar_width, list(X.columns))
+    plt.xlabel("Features")
+    plt.ylabel("Mean value in cm")
+    plt.legend(bbox_to_anchor=(1, 1))
+    plt.savefig("barplot.png", dpi=300, bbox_inches="tight")
 
-data = df.values
-X = data[:, :4]
-Y = data[:, 4]
 
-# bar plots
-bar_width = 0.2
-mean_values = np.array([np.average(X[:,i][Y==j]) for i in range(X.shape[1]) for j in np.unique(Y)])
-reshaped = mean_values.reshape(4,3)
-reshaped = np.swapaxes(reshaped,0,1)
-x_axis  =  np.arange(len(columns)-1)
-plt.bar(x_axis, reshaped[0], bar_width, label="Setosa")
-plt.bar(x_axis + bar_width, reshaped[1], bar_width, label="Versicolor")
-plt.bar(x_axis + bar_width * 2, reshaped[2], bar_width, label="Virginica")
-plt.xticks(x_axis + bar_width, columns[:4])
-plt.xlabel("Features")
-plt.ylabel("Mean value in cm")
-plt.legend(bbox_to_anchor=(1,1))
-plt.savefig('barplot.png', dpi=300, bbox_inches='tight')
+def svm_classification():
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    svc = SVC()
+    svc.fit(X_train, y_train)
+    print(f"SVM classification score: {svc.score(X_test, y_test)}")
+
+
+plot_pairplots()
+plot_barplots()
+svm_classification()
