65333810cf9e90d4dd328e3854d40088334e8a21
Julian Alvarez <juroalwi@gmail.com>
2026-03-19 08:35:45 -0300

    add tree clasifier with plot

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ import numpy as np
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 
 data = sns.load_dataset("iris")
 X = data.drop("species", axis=1)
@@ -36,6 +37,24 @@ def svm_classification():
     print(f"SVM classification score: {svc.score(X_test, y_test)}")
 
 
-plot_pairplots()
-plot_barplots()
+def tree_classification_with_plot():
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    dtc = DecisionTreeClassifier()
+    dtc.fit(X_train, y_train)
+    plt.figure()
+    ax = sns.scatterplot(
+        x=X["petal_width"],
+        y=X["petal_length"],
+        hue=data["species"],
+        style=dtc.predict(X),
+    )
+    ax.set_title("Real labels")
+    ax.legend(title="Real labels and predicted labels")
+    plt.savefig("tree-classification.png", dpi=300, bbox_inches="tight")
+    print(f"Decision tree classification score: {dtc.score(X_test, y_test)}")
+
+
+# plot_pairplots()
+# plot_barplots()
 svm_classification()
+tree_classification_with_plot()
